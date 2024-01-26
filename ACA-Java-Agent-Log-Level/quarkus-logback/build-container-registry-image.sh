2d051534614ced1377f39615956d411f7bc7635a
hui1110 <hui1110rui@163.com>
2024-01-26 16:17:04 +0800

    rollback

--- ACA-Java-Agent-Log-Level/quarkus-logback/build-container-registry-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-az acr login --name $ACR_NAME  --expose-token
-
-ll
-
-az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:quarkus-logback:v1 .
--- ACA-Java-Agent-Log-Level/spring-log4j2/src/main/java/com/microsoft/azure/spring/accelerator/log4j2/controller/HelloController.java
+++ ACA-Java-Agent-Log-Level/spring-log4j2/src/main/java/com/microsoft/azure/spring/accelerator/log4j2/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello world!");
-        logger.debug("Hello world, spring-log4j2, jdk 21");
+        logger.debug("Hello world");
         return "hello";
     }
 
