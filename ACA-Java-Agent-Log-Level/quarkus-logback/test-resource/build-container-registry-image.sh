b1b928d0341d0468cbc566e8367993bdef28d1bd
hui1110 <hui1110rui@163.com>
2024-01-26 16:58:33 +0800

    update sh

--- ACA-Java-Agent-Log-Level/quarkus-logback/test-resource/build-container-registry-image.sh
+++ ACA-Java-Agent-Log-Level/quarkus-logback/test-resource/build-container-registry-image.sh
@@ -4,6 +4,4 @@ set -Eeuo pipefail
 
 az acr login --name $ACR_NAME  --expose-token
 
-ll
-
 az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:quarkus-logback:v1 https://github.com/hui1110/JavaLogTestProject.git -file ACA-Java-Agent-Log-Level/quarkus-logback/Dockerfile
