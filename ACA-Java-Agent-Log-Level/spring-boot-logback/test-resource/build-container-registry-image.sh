65915d5cb863e0045c9ea6f6fd6991a7ab68c141
hui1110 <hui1110rui@163.com>
2024-01-29 14:17:06 +0800

    add arm

--- ACA-Java-Agent-Log-Level/spring-boot-logback/test-resource/build-container-registry-image.sh
+++ ACA-Java-Agent-Log-Level/spring-boot-logback/test-resource/build-container-registry-image.sh
@@ -6,4 +6,6 @@ az acr login --name $ACR_NAME  --expose-token
 
 git clone https://github.com/hui1110/JavaLogTestProject.git
 
-az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:spring-boot-logback --file JavaLogTestProject/ACA-Java-Agent-Log-Level/spring-boot-logback/Dockerfile JavaLogTestProject/ACA-Java-Agent-Log-Level/spring-boot-logback
+cd JavaLogTestProject/ACA-Java-Agent-Log-Level/spring-boot-logback
+
+az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:spring-boot-logback .
