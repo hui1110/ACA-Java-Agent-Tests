0359eacd578f97261dfb57c356989d63813a2b6a
hui1110 <hui1110rui@163.com>
2024-01-29 17:16:16 +0800

    try

--- ACA-Java-Agent-Log-Level/spring-boot-logback/test-resource/build-container-registry-image.sh
+++ ACA-Java-Agent-Log-Level/spring-boot-logback/test-resource/build-container-registry-image.sh
@@ -6,4 +6,4 @@ cp -f ../../../spring-boot-docker/Dockerfile ../../../Dockerfile
 
 az acr login --name $ACR_NAME  --expose-token
 
-az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:$PROJECT_NAME --file spring-boot-docker/Dockerfile --build-arg PROJECT_NAME=$PROJECT_NAME https://github.com/hui1110/JavaLogTestProject.git
+az acr build --registry $ACR_NAME --image $ACR_NAME/asa-agent:$PROJECT_NAME --build-arg PROJECT_NAME=$PROJECT_NAME https://github.com/hui1110/JavaLogTestProject.git
